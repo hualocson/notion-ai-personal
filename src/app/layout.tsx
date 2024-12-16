960ecf3df19c5fe21da43e1e5283fd14c6322c9a
locson <hualocson@gmail.com>
2024-12-16 19:42:19 +0700

    UPDATE fix error import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 
-import { cn } from "@/src/lib/utils";
+import { cn } from "@/lib/utils";
 
 import "./globals.css";
 
