56c85c5ddc76412a52e9ffd433cba14b652c6070
locson <hualocson@gmail.com>
2024-12-19 22:14:53 +0700

    UPDATE new page ui

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,23 +1,8 @@
 "use client";
 
-import MagicInput from "./components/magic-input";
-import ProcessedPrompt from "./components/processed-prompt";
-
-const sampleData = {
-  name: "Eating",
-  date: "2024-12-18",
-  amount: 20000000,
-  account: "bidv",
-};
 const Dashboard = () => {
   return (
-    <div className="mx-auto flex h-[calc(100svh-110px)] w-full max-w-screen-sm flex-col-reverse items-center gap-6 lg:h-[calc(100dvh-120px)]">
-      <div className="w-full">
-        <MagicInput />
-      </div>
-
-      <ProcessedPrompt {...sampleData} />
-    </div>
+    <div className="mx-auto flex h-[calc(100svh-110px)] w-full max-w-screen-sm flex-col-reverse items-center gap-6 lg:h-[calc(100dvh-120px)]"></div>
   );
 };
 
