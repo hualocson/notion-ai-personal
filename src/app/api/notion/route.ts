22c19564035b7eae2c91398eb3117764cfb436fa
locson <hualocson@gmail.com>
2024-12-22 20:54:33 +0700

    UPDATE new route

--- src/app/api/notion/route.ts
+++ src/app/api/notion/route.ts
@@ -1,9 +1,7 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const notionToken = process.env.NOTION_TOKEN;
   return NextResponse.json({
     message: "Hello from the API route!",
-    token: notionToken,
   });
 }
