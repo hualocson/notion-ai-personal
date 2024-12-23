2dfc3249de16a9fdc22f15cca19ec3c3b7f5537b
locson <hualocson@gmail.com>
2024-12-23 23:23:29 +0700

    ADD revalidate manual button

--- src/app/dashboard/components/dashboard-content-layout.tsx
+++ src/app/dashboard/components/dashboard-content-layout.tsx
@@ -1,6 +1,10 @@
 "use client";
 
 import { cn } from "@/lib/utils";
+import { useIsFetching, useQueryClient } from "@tanstack/react-query";
+import { Loader2, RotateCcw } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
 
 const DashboardContentLayout = ({
   children,
@@ -11,13 +15,34 @@ const DashboardContentLayout = ({
   headerText?: string;
   className?: string;
 }>) => {
+  const queryClient = useQueryClient();
+  const handleOnRefresh = () => {
+    queryClient.invalidateQueries({
+      queryKey: ["accounts"],
+    });
+  };
+
+  // check is query is fetching
+  const isFetching = useIsFetching({ queryKey: ["accounts"] }) > 0;
+
   return (
     <>
       <header className="container fixed left-1/2 top-0 z-50 mx-auto flex h-16 w-full max-w-screen-sm -translate-x-1/2 items-center justify-between bg-background/20 backdrop-blur-lg">
         <p className="text-lg font-bold">{headerText}</p>
-        <span className="inline-flex size-10 items-center justify-center rounded-full bg-indigo-400 font-bold text-white">
-          S
-        </span>
+        <Button
+          className="inline-flex size-10 items-center justify-center rounded-full bg-indigo-400/10 font-bold text-indigo-400 hover:bg-indigo-400/30"
+          onClick={handleOnRefresh}
+          disabled={isFetching}
+        >
+          {
+            // show loading icon if fetching
+            isFetching ? (
+              <Loader2 className="animate-spin" size={16} />
+            ) : (
+              <RotateCcw size={16} />
+            )
+          }
+        </Button>
       </header>
       <div
         className={cn(
