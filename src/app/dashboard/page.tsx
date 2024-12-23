178e9992777d00f5689455f608264d7294c38f91
locson <hualocson@gmail.com>
2024-12-23 20:56:23 +0700

    UPDATE add display link after create page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,10 +2,12 @@
 
 import { useState } from "react";
 
+import Link from "next/link";
+
 import { cn } from "@/lib/utils";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import { Loader2, RotateCcw } from "lucide-react";
+import { Link2, Loader2, RotateCcw } from "lucide-react";
 import { toast } from "sonner";
 
 import { Button } from "@/components/ui/button";
@@ -17,7 +19,7 @@ import MagicInput from "./components/magic-input";
 import TransactionAIOutput from "./components/transaction-ai-output ";
 
 const Dashboard = () => {
-  const { data, isPending, isFetching } = useQuery({
+  const { data, isPending } = useQuery({
     queryKey: ["accounts"],
     queryFn: () =>
       axios
@@ -28,7 +30,10 @@ const Dashboard = () => {
         .then((res) => res.data),
   });
 
-  const [prompt, setPrompt] = useState<string>();
+  const [promptData, setPromptData] = useState<{
+    prompt?: string;
+    url?: string;
+  }>();
   const [output, setOutput] = useState<IGeminiOutput>();
   const aiCallMutation = useMutation({
     mutationFn: (inputValue: string) =>
@@ -52,40 +57,55 @@ const Dashboard = () => {
       return;
     }
     setOutput(undefined);
-    setPrompt(inputValue);
+    setPromptData({
+      prompt: inputValue,
+      url: undefined,
+    });
     aiCallMutation.mutate(inputValue.trim());
   };
 
   const accounts = data?.data ?? [];
 
+  const onAddPageSuccess = (url: string) => {
+    setPromptData((prev) => ({ ...prev, url }));
+  };
+
   return (
     <DashboardContentLayout headerText="Dashboard">
-      {isPending || isFetching ? (
+      {isPending ? (
         <div className="inline-flex size-full min-h-[118px] items-center justify-center">
           <Loader2 className="size-4 animate-spin" />
         </div>
       ) : (
         <div className="container">
           <AccountCarousel data={accounts} />
-          {/* <AccountListing data={data?.data ?? []} /> */}
         </div>
       )}
 
       <div className="container mt-6 flex w-full flex-col gap-4">
-        {prompt && (
+        {promptData && (
           <div className="flex w-full items-center gap-2 text-start">
             <div className="flex items-center gap-2">
               <span className={cn(aiCallMutation.isPending && "animate-pulse")}>
-                {prompt}
+                {promptData.prompt}
               </span>
+              {promptData.url && (
+                <Link
+                  href={promptData.url}
+                  target="_blank"
+                  className="flex size-8 items-center justify-center rounded-lg bg-teal-300/10 text-teal-400"
+                >
+                  <Link2 className="size-4" />
+                </Link>
+              )}
               {aiCallMutation.isPending && (
                 <AIIndicator className="ml-4 size-6" />
               )}
-              {prompt && !aiCallMutation.isPending && (
+              {promptData.prompt && !aiCallMutation.isPending && (
                 <Button
                   size={"icon"}
-                  className="size-6 bg-amber-300/10 text-amber-600 hover:bg-amber-300/40 [&_svg]:size-3"
-                  onClick={() => handleOnCallAI(prompt)}
+                  className="size-8 bg-amber-300/10 text-amber-600 hover:bg-amber-300/40 [&_svg]:size-4"
+                  onClick={() => handleOnCallAI(promptData.prompt!)}
                 >
                   <RotateCcw />
                 </Button>
@@ -93,7 +113,12 @@ const Dashboard = () => {
             </div>
           </div>
         )}
-        {output && <TransactionAIOutput data={output} />}
+        {output && (
+          <TransactionAIOutput
+            data={output}
+            onAddPageSuccess={onAddPageSuccess}
+          />
+        )}
       </div>
 
       <div className="fixed inset-x-6 bottom-12 mx-auto max-w-screen-sm">
